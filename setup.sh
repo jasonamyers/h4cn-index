59ca6ae4b530e8388ff4ebffd4f87062396aa568
Jason Myers <jason@jasonamyers.com>
2013-06-01 10:16:54 -0500

    Add initial setup script
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# clear screen
+clear
+
+# start with a clear screen
+cd ~/src/GlucoseTrack
+
+# source the virtual environment
+source venv/bin/activate
+
+# make sure any new requirements have been installed
+pip install -r requirements.txt
+
+# clear install info if desired
+clear
+
+#if [-f /tmp/test.db ]
+#then
+    #rm /tmp/test.db
+#fi
+
+export DATABASE_URL="sqlite:////tmp/test.db"
+
+# setup database inside of project
+python -c "from app import db; db.create_all()"
+
+# run app
+python run.py
