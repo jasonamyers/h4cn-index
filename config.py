cada7793d38fd8bd4d4c871689bd12e5b60681a5
Jason Myers <jason@jasonamyers.com>
2013-06-01 14:08:09 -0500

    Adding passlib to requirements and the SQLAlchemy URI
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ DEBUG = False
 
 ADMINS = frozenset(['', ])
 SECRET_KEY = 'ksvnnaJcUH2D'
+SQLALCHEMY_DATABASE_URI = 'psycopg2+postgres://u2m0jgp68pevsd:p390mu1qaogbrb6hhmnil46sukd@ec2-23-21-193-88.compute-1.amazonaws.com:5432/d3ibj7prohvebf'
 SQLALCHEMY_POOL_RECYCLE = 30
 
 CSRF_ENABLED = True
