f97a42fb08a7f7c64da4e184dc77c1cf304cc4a7
Jason Myers <jason@jasonamyers.com>
2013-06-01 10:11:40 -0500

    Added initial flask base
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

---
+++ app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from flask.ext.sqlalehcmy import SQLAlchemy
+from flask.ext.login import LoginManager, current_user
+from flask.ext.pricipal import (Principal, Permission, identity_loaded, RoleNeed, UserNeed)
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
+principals = Principal(app)
+
+admin_permission = Permission(RoleNeed('admin'))
+
+from app.users.models import Users
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return Users.by_id(userid)
+
+
+@app.errorhandler(404)
+def non_fount(error):
+    return render_template('404.html'), 404
+
+
+@app.route('/', method=['GET', ])
+def handle_splash():
+    return render_template('index.html')
+
+from flu.users.views import mod as usersModule
+app.register_blueprint(usersModule)
+
+
+@identity_loaded.connect_via(app)
+def on_identity_loaded(sender, identity):
+    identity.user = current_user
+
+    if hasattr(current_user, 'id'):
+        identity.providers.add(UserNeed(current_user.id))
+
+    if hasattr(current_user, 'roles'):
+        for role in current_user.roles:
+            identity.provides.add(RoleNeed(role.name))
--- app/users/views.py
+++ app/users/views.py
@@ -1,24 +1,25 @@
 import random
 
 from sqlalchemy.exc import IntegrityError
-from passlib.hash import pdkdf2_sha256
+from passlib.hash import pbkdf2_sha256
 
 from flask import (Blueprint, request, render_template, flash, g, session,
     redirect, url_for, json, jsonify, current_app)
 from flask.ext.login import (login_user, logout_user, current_user)
-from flask.ext.pricipal import Identity, AnonymousIdentity, indentity_changed
+from flask.ext.pricipal import Identity, AnonymousIdentity, identity_changed
 
 from app import db
 from app.users.models import Users, Roles
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
+
 @mod.before_request
 def before_request():
     g.user = None
     if 'user_id' in session:
         g.user = Users.by_id(session['user_id'])
-        
+
 
 @mod.route('/login', methods=['GET', 'POST', ])
 def login():
@@ -29,7 +30,6 @@ def login():
             user = Users.by_email(request.form.get('username', ''))
         else:
             user = Users.by_username(request.form.get('username', ''))
-            
         if user:
             if pbkdf2_sha256.verify(request.form.get('password', ''), user.password):
                 session['user_id'] = user.id
@@ -38,4 +38,28 @@ def login():
                 identity_changed.send(current_app._get_current_object(),
                     identity=Identity(user.id))
 
-                return "Stuff"
+                return redirect(url_for('STUFF'))
+        else:
+            return redirect(url_for('handle_splash'))
+
+
+@mod.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('users/register.html')
+    elif request.method == 'POST':
+        name = request.form.get('name', '')
+        username = request.form.get('username', '')
+        email = request.form.get('email', '')
+        password = pbkdf2_sha256.encrypt(request.form.get('password', ''), rounds=8000, salt_size=10)
+        user = Users(name=name, username=username, email=email, password=password)
+        #  TODO: Make more flexible based on the UI for this view
+        if request.form.get('role', '') == "2":
+            role = Roles.by_id(2)
+            user.roles.append(role)
+        db.session.add(user)
+        db.session.commit()
+        if not current_user.is_anonymous():
+            return redirect(url_for('STUFF'))
+        else:
+            return redirect(url_for('handle_splash'))
