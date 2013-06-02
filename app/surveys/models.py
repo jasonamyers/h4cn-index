60dd8f7a9bee6031fd1b7cadca555972dcbf81b0
Brandon Bradley <crixdev@gmail.com>
2013-06-01 21:43:23 -0500

    Added constructors to Surveys model

--- app/surveys/models.py
+++ app/surveys/models.py
@@ -3,8 +3,17 @@ from app import db
 class Surveys(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    desc = db.Column(db.String(1000))
     questions = db.relationship('Question', backref='question', lazy='dynamic')
+
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
 
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1000))
+
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
